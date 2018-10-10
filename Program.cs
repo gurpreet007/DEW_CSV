3e9bdffb52b90a265e4740f34add72ad9e386d07
Gurpreet Singh <gurpreet007@gmail.com>
2018-10-10 23:34:57 +1030

    Major refactoring and other minor changes

--- Program.cs
+++ Program.cs
@@ -11,18 +11,16 @@ namespace ConsoleApp1
   {
     private static void Print(string message, string type="Info")
     {
-      ConsoleColor consoleColor = Console.ForegroundColor;
       if (type == "Error")
       {
         message = "[!] " + message;
         Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine(message);
       }
-      else
-      {
-        Console.WriteLine(message);
-      }
-      Console.ForegroundColor = consoleColor;
+      else if (type == "Success")
+        Console.ForegroundColor = ConsoleColor.Green;
+
+      Console.WriteLine(message);
+      Console.ResetColor();
     }
     private static bool DownloadFile(string strUrl, string fileName)
     {
@@ -38,72 +36,105 @@ namespace ConsoleApp1
       }
       return true;
     }
-    static void Main(string[] args)
+    private static bool CreateNewFile(string OrigFile, string NewFile)
     {
-      const string URL = @"https://apps.waterconnect.sa.gov.au/file.csv";
-      const string origFile = "file.csv";
-      const string newFile = "newFile.csv";
-      bool isDownloaded = DownloadFile(URL, origFile);
+      const int ExpectedCols = 20;
+      const int IndxUnitNo = 7;
+      const int IndxSwl = 9;
+      const int IndxRswl = 10;
+      bool isHeader = true;
+      int lineNum = 0;
+      string line;
 
-      if (isDownloaded)
-        Print("File Downloaded Successfully");
-      else
-        Print("Error Downloading File", "Error");
-
-      using (StreamWriter writer = new StreamWriter(newFile))
+      Print("\n\nCreating new file...");
+      using (StreamWriter writer = new StreamWriter(NewFile))
       {
-        using (StreamReader reader = new StreamReader(origFile))
+        using (StreamReader reader = new StreamReader(OrigFile))
         {
-          string line;
-          const int INDX_UNITNO = 7;
-          const int INDX_SWL = 9;
-          const int INDX_RSWL = 10;
-          string swl, rswl;
-          bool header = true;
-          int count = 1;
           while ((line = reader.ReadLine()) != null)
           {
+            lineNum++;
             List<string> cols = line.Split(',').ToList<string>();
 
-            //Assuming that comment will not contain newline
-            //Skipping any line with less than 20 columns
-            if (cols.Count < 20)
+            // Assuming that comment will not contain newline.
+            // Skipping any line with less than 20 columns.
+            if (cols.Count < ExpectedCols)
             {
-              Print(string.Format("Skipping line:{0}, cols: {1} ", count.ToString(), cols.Count), "Error");
+              Print(string.Format("Skipping line:{0}, cols: {1} ", lineNum.ToString(), cols.Count), "Error");
               continue;
             }
 
-            //Number of columns can be more if comments column contain commas
-            //As comment column is at last, wrapping up all extra columns as a single column
-            while(cols.Count>20)
+            // Number of columns can be more if comment column contain commas.
+            // As comment column is at last, wrapping up all extra columns as a single column.
+            while(cols.Count > ExpectedCols)
             {
               cols[cols.Count - 2] += "," + cols[cols.Count - 1];
               cols.RemoveAt(cols.Count - 1);
             }
 
-            //Removing Unit No column
-            cols.RemoveAt(INDX_UNITNO);
-
-            if (header)
+            // Check for and put the closing quote character if necessary, needed to handle newline in comment (e.g. line 8079)
+            int lastCol = cols.Count - 1;
+            string strLastCol = cols[lastCol];
+            if (strLastCol != "")
             {
-              header = false;
+              char lastColFirstChar = strLastCol[0];
+              char lastColLastChar = strLastCol[strLastCol.Length - 1];
+              if (lastColFirstChar == '"' && lastColLastChar != '"')
+              {
+                cols[lastCol] += '"';
+              }
+            }
+
+            // Removing Unit No column.
+            cols.RemoveAt(IndxUnitNo);
+
+            if (isHeader)
+            {
+              isHeader = false;
               cols.Add("calc");
             }
             else
             {
-              swl = cols[INDX_SWL];
-              rswl = cols[INDX_RSWL];
-              if (!double.TryParse(swl, out double dbl_swl)) dbl_swl = 0.0;
-              if (!double.TryParse(rswl, out double dbl_rswl)) dbl_rswl = 0.0;
+              // Calculating the value for new calc column
+              if (!double.TryParse(cols[IndxSwl], out double dbl_swl))
+                dbl_swl = 0.0;
+
+              if (!double.TryParse(cols[IndxRswl], out double dbl_rswl))
+                dbl_rswl = 0.0;
+
               cols.Add((dbl_swl + dbl_rswl).ToString());
             }
-            string newLine = string.Join(",", cols);
-            writer.WriteLine(newLine);
-            count++;
+
+            writer.WriteLine(string.Join(",", cols));
           }
         }
       }
-      Print("New file saved at: " + (new System.IO.FileInfo(newFile)).FullName);
+      Print("\nNew file created.");
+      return true;
+    }
+    static void Main(string[] args)
+    {
+      const string Url = @"https://apps.waterconnect.sa.gov.au/file.csv";
+      const string OrigFile = "file.csv";
+      const string NewFile = "newFile.csv";
+
+      // Downloading file from specified url.
+      if (DownloadFile(Url, OrigFile))
+        Print("File Downloaded Successfully");
+      else
+      {
+        Print("\nProgram Finished. Press Enter to exit.");
+        Console.ReadLine();
+        return;
+      }
+
+      // Creating new file
+      CreateNewFile(OrigFile, NewFile);
+
+      Print("\nNew file saved at: " + (new System.IO.FileInfo(NewFile)).FullName, "Success");
+
+      Print("\nProgram Finished. Press Enter to exit.");
+      Console.ReadLine();
     }
   }
 }
